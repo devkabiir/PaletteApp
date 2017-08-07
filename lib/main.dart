81f4760f59a7c33c709f7e6a2c154920a94341ee
Andrew Eskild Christensen <andreweskild@gmail.com>
2017-08-07 13:49:03 -0700

    Created basic MainApp Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,20 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(new MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Palette',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Palettes')
+        ),
+        body: new Text('My Body')
+      ),
+    );
+  }
+}
